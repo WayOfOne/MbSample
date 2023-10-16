35ffc0c9aecd024cf3749a15d41fdaf8f3e8ebfa
Don Lao <thewayofone@gmail.com>
2023-10-16 09:50:23 -0700

    added mb sim generator

---
+++ src/MbSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using MbSample.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MbSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHubContext<WeatherHub> _hubContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHubContext<WeatherHub> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWeatherForecast(WeatherForecast forecast)
+        {
+            if (forecast == null)
+            {
+                return BadRequest();
+            }
+
+            await _hubContext.Clients.All.SendAsync("WeatherUpdated", forecast);
+
+            return Ok(forecast);
+        }
+
+    }
+}
